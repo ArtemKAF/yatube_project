57f9e1e21f95ad4f8d6a7b30ba70e6223e7e5919
Kozin Artem <kaf-artem@yandex.ru>
2022-11-29 15:11:47 +0300

    Created model Group. Registered model Group in admin.py. Migrate done

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Create your models here.
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -11,4 +19,11 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts',
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='groups',
+        blank=True,
+        null=True,
     )
