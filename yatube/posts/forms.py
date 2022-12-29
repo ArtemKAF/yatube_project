a170af69f6f40baa93d3d5b088a0017f6c78344c
Kozin Artem <kaf-artem@yandex.ru>
2022-12-29 07:19:41 +0300

    Cleaned unnecessary code

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,22 +1,9 @@
 from django import forms
 
-from .models import Group, Post
+from .models import Post
 
 
 class PostForm(forms.ModelForm):
-
-    text = forms.CharField(
-        label=Post._meta.get_field("text").verbose_name,
-        help_text=Post._meta.get_field("text").help_text,
-        widget=forms.Textarea(),
-    )
-    group = forms.ModelChoiceField(
-        queryset=Group.objects.all(),
-        label=Post._meta.get_field("group").verbose_name,
-        help_text=Post._meta.get_field("group").help_text,
-        required=False,
-    )
-
     class Meta:
         model = Post
 
