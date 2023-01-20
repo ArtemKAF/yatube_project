148b257c9e5c5eee5611f380755aaf527a8b0981
Kozin Artem <kaf-artem@yandex.ru>
2023-01-20 03:20:49 +0300

    Fixed flake8 errors

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -73,8 +73,8 @@ def profile(request, username):
     author = get_object_or_404(User, username=username)
     following = False
     if (request.user.is_authenticated
-        and request.user.follower.filter(author=author).exists()):
-            following = True
+            and request.user.follower.filter(author=author).exists()):
+        following = True
     context = {
         "page_obj": create_pagination(
             author.posts.select_related("group"),
